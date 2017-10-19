1f1c2724ab1ed58119a04e488d43c58a205debc6
Daniel Capacio <danielcapacio@gmail.com>
2017-10-19 02:02:14 -0700

    Fade in / fade out UI extensions class

---
+++ ReadingCalendarEntries/UIViewExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewExtensions.swift
+//  ReadingCalendarEntries
+//
+//  Created by Daniel Capacio on 2017-10-19.
+//  Copyright © 2017 Daniel Capacio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/*
+ * Fade In / Out Animations as Class Extensions in Swift
+ */
+extension UIView {
+    func fadeIn(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)  }
+    
+    func fadeOut(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 0.0
+        }, completion: completion)
+    }
+}
